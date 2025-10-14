96f9e8838215d37dcf501bf1f61c7c920336e275
animogok <sebastian.avenda32@gmail.com>
2025-10-13 19:25:33 -0500

    Fix: Dado un problema con la generacion del algoritmo de NQueens, se establece que los tiempos sean entregados en un formato RAW siendo el RAW los Nanosegundos totales

--- CS Works/Task 6/main.cpp
+++ CS Works/Task 6/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <sstream>
 #include <windows.h>
 #include <chrono>
 #include "NQueens.h"
@@ -18,7 +16,7 @@ int main() {
     auto limite24h = tiempoInicio + std::chrono::hours(24);
     size_t n = 1;
 
-    while (n < 20) {
+   while (std::chrono::steady_clock::now() < limite24h && n < 20){
         TorreHanoi torre(n);
 
         LARGE_INTEGER freq, start, end;
@@ -31,33 +29,13 @@ int main() {
         unsigned long long ticks = static_cast<unsigned long long>(end.QuadPart - start.QuadPart);
         unsigned long long ns = ticks * 1000000000ULL / freq.QuadPart;
 
-        unsigned long long total_seconds = ns / 1000000000ULL;
-        unsigned long long hours = total_seconds / 3600ULL;
-        unsigned long long minutes = (total_seconds % 3600ULL) / 60ULL;
-        unsigned long long seconds = total_seconds % 60ULL;
-
-        unsigned long long msecs = (ns / 1000000ULL) % 1000ULL;
-        unsigned long long nsrem = ns % 1000000ULL;
-        unsigned long long usecs = nsrem / 1000ULL;
-        unsigned long long nsecs = nsrem % 1000ULL;
-
-        std::ostringstream oss;
-        oss << std::setfill('0') << std::setw(2) << hours << ":"
-            << std::setw(2) << minutes << ":"
-            << std::setw(2) << seconds << ":"
-            << std::setw(3) << msecs << ":"
-            << std::setw(3) << usecs << ":"
-            << std::setw(3) << nsecs;
-
-        archivoNQueens << oss.str() << "\n";
+        archivoNQueens << ns << "\n";
         archivoNQueens.flush();
-        std::cout << "Tiempo Torre Hanoi (N=" << n << "): " << oss.str() << std::endl;
+
+        std::cout << "Tiempo Torre Hanoi (N=" << n << "): " << ns << " ns" << std::endl;
         n++;
     }
 
     archivoNQueens.close();
     return 0;
 }
-
-
-
